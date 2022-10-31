82cd085f53f679b601c106dfc39eaa32ae3fb63f
TonmoyDas5 <tonmoydas3600@gmail.com>
2022-10-31 12:11:42 +0600

    php commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?p
+//echo "This is my first coding for p";
+
+//$a=10;
+//echo $a;
+
+// $b=20;
+// echo $b;
+// $c=30;
+// echo $c;
+
+//arithmatic oparetor
+//+,_,*,/,%,=
+
+
+
+// $a=3;
+// $b=4;
+
+// echo $a/$b;
+
+// asignment oparetor
+// +=,-=,*=,/=,%=
+// $x=12;
+// $y=15;
+
+// $z=$x+$y;
+s// echo $z;
+
+
+//conditional oparetor
+//if,elseif,else
+
+$q=50;
+$p=20;
+$r=30;
+
+if($q<$p){
+    echo "ami bul";
+}
+elseif($q<$r){
+ echo "ami right";
+}
+else{
+    echo "ggg";
+}
+
+
+
+
+
+
+
+
+
+
+?>
